4828ad3d57462f236e72b225435ee6ff65bc5e6e
Md. Samiul Islam Sagar <50207775+Samiul1403001@users.noreply.github.com>
2026-01-05 13:51:35 -0600

    Add files via upload

---
+++ EISmeasurement_trial02.py
@@ -0,0 +1,47 @@
+from MyDigilent import MyDigilent, clean_buffer, freq_selection_signal
+from time import sleep
+import numpy as np
+
+FREQ = [1, 2, 5, 10]
+
+FREQ = np.round(FREQ, decimals=2)
+
+# UART specs
+PIN_TX = 0           # DIO pin used for UART TX (ADP3450 DIO0)
+PIN_RX = 1           # optional RX pin if you want to read back
+BAUDRATE = 115200
+# Device object generation
+Digi_1 = MyDigilent(tx=PIN_TX,
+                    rx=PIN_RX,
+                    baud_rate=BAUDRATE,
+                    parity="none",
+                    data_bits=8,
+                    stop_bits=1)
+
+Digi_1.scope_setup(channels=[1, 2])
+
+sleep(1)
+
+# Main loop
+try:
+    sample = np.zeros([61, 3])
+    i = 0
+    for f in FREQ:
+        CMD = str(f)
+        Digi_1.sendStringUART(CMD)
+        sleep(0.5)
+        mainloop = True
+        while mainloop == True:
+            RES = bytes(Digi_1.uart_read())
+            if RES.decode("utf-8") == "Received":
+                sleep(0.5)
+                print(f"\nMeasuring EIS at {CMD.strip()} Hz...")
+                while bytes(Digi_1.uart_read()).decode("utf-8") != "Done": pass
+                mainloop = False
+
+    Digi_1.close()
+    print("Device closed.")
+
+except KeyboardInterrupt:
+    Digi_1.close()
+    print("Device closed.")
