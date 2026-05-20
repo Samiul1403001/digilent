41584fc8a1c15f80ff68901f860bddf24b8d634b
Md. Samiul Islam Sagar <50207775+Samiul1403001@users.noreply.github.com>
2026-05-20 16:16:08 -0500

    Add files via upload

--- DigilentEISFinal/MyDigilent.py
+++ DigilentEISFinal/MyDigilent.py
@@ -138,7 +138,7 @@ def freq_selection_signal(y_buffer, freq_sweep, sample_rate):
     if freq_sweep[0]/0.98 >= 0.5:
         freq_int = 0.1 * freq_sweep[0]/0.98
     else:
-        freq_int = 2e-5 * freq_sweep[0]/0.98
+        freq_int = 1e-4 * freq_sweep[0]/0.98
 
     for f in np.arange(freq_sweep[0], freq_sweep[1], freq_int):
         _, params = clean_buffer(y_buffer, f, sample_rate)
